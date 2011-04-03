49e7e2e6e79fdacc1b08ec14a592befc4e4e16ae
morbidflame <p.fenaroli@gmail.com>
2011-04-03 11:36:16 +0200

    primo commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '0b640f595e5687a07231aeeadedb4a067aa5528a4781e119a7f2f4bcd7f4149a8974ea3ad1ce25a943fb5b1114b0bcfc58945101a59b7793d19b505819e854ec'
